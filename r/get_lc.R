4b2de77eb01e1f9adfd4d09ff2ab854ae382a1c1
‘admahood’ <admahood@gmail.com>
2020-10-22 17:38:33 +0000

    lc

--- r/get_lc.R
+++ r/get_lc.R
@@ -4,10 +4,6 @@ system("aws s3 sync s3://earthlab-natem/modis-burned-area/input/landcover/MCD12Q
 
 library(sf);library(tidyverse); library(raster)
 install.packages("exactextractr");library(exactextractr)
-getmode <- function(v) {
-  uniqv <- na.omit(unique(v))
-  uniqv[which.max(tabulate(match(v, uniqv)))]
-}
 
 download.file("https://ldas.gsfc.nasa.gov/sites/default/files/ldas/gldas/VEG/GLDASp4_domveg_025d.nc4",
               "data/gldas.nc4")
@@ -20,9 +16,11 @@ na <- st_read("data/fired_na_2017-nids.gpkg") %>%
 res<-list()
 for(y in 1:length(years)){
   lcfiles <- list.files("data/MCD12Q1_mosaics/", full.names = TRUE)
+  r <- raster(lcfiles[[y]])
+  r[r==0] <- NA
   res[[y]] <- na %>%
     filter(lc_year == years[y]) %>%
-    mutate(lc = exact_extract(x=raster(lcfiles[[y]]),y= ., 'mode'))
+    mutate(lc = exact_extract(x=r,y= ., 'mode'))
 }
 
 na_lc<- do.call('rbind',res) %>%
@@ -38,13 +36,15 @@ sa <- st_read("data/fired_sa_2017-nids.gpkg") %>%
 res<-list()
 for(y in 1:length(years)){
   lcfiles <- list.files("data/MCD12Q1_mosaics/", full.names = TRUE)
+  r <- raster(lcfiles[[y]])
+  r[r == 0] <- NA
   res[[y]] <- sa %>%
     filter(lc_year == years[y]) %>%
-    mutate(lc = exact_extract(x=raster(lcfiles[[y]]),y= ., 'mode'))
+    mutate(lc = exact_extract(x=r,y= ., 'mode'))
 }
 
 sa_lc<- do.call('rbind',res) %>%
   dplyr::select(-lc_year)%>%
   mutate(gldas = exact_extract(x=gldas, y=., 'mode'))
-st_write(sa_lc, "data/fired_sa_2017-nids_lc.gpkg")
+st_write(sa_lc, "data/fired_sa_2017-nids_lc.gpkg", delete_dsn = TRUE)
 system("aws s3 cp data/fired_sa_2017-nids_lc.gpkg s3://earthlab-amahood/night_fires/fired_sa_2017-nids_lc.gpkg")
