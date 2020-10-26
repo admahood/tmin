b5beea2320b120968abcb495ff14edfe03f3d36b
‘admahood’ <admahood@gmail.com>
2020-10-26 19:11:00 +0000

    k

--- r/goes_exract.r
+++ r/goes_exract.r
@@ -1,6 +1,6 @@
 # extract GOES16 data to fired polygons
 
-libs<- c("tidyverse", "exactextractr", "sf", "lubridate", "data.table", 
+libs<- c("tidyverse", "sf", "lubridate", "data.table", 
          "doParallel", "foreach")
 invisible(sapply(libs, library, character.only=TRUE, quietly=TRUE))
 
@@ -12,19 +12,18 @@ system("aws s3 sync s3://earthlab-mkoontz/goes16 data/goes16 --only-show-errors"
 # fired polygons
 system("aws s3 sync s3://earthlab-amahood/night_fires/lc_splits data/fired --only-show-errors")
 
-goes_files<- list.files("data/goes16", pattern = ".csv") %>%
+goes_files<- list.files("data/goes16", pattern = ".csv", full.names = TRUE) %>%
   as_tibble() %>%
   dplyr::rename(filename = value) %>%
   separate(filename, c("datetime", "datetime_mid"), sep = "_", remove=FALSE) %>%
-  mutate(date = str_sub(datetime,1,8) %>% as.Date(date, format="%Y%m%d"),
-         hour = str_sub(datetime, 9, 10))
+  mutate(date = str_sub(datetime,13,20) %>% as.Date(date, format="%Y%m%d"))
 fired_files <- list.files("data/fired", pattern = ".gpkg", full.names = TRUE)
 dir.create("data/out")
 
 corz <- detectCores()-1
 registerDoParallel(corz)
 
-foreach(i = 1:length(fired_files))%dopar%{
+for(i in 1:length(fired_files)){
   
   fired<- st_read(fired_files[i])
   
@@ -34,39 +33,39 @@ foreach(i = 1:length(fired_files))%dopar%{
   
   fire_counts <- list()
   counter<-1
-  for(f in 1:nrow(fired)){
+  system(paste("echo", out_file))
+  
+  fc<-foreach(f = 1:nrow(fired), .combine= bind_rows)%dopar%{
+    
     goes <- goes_files %>%
       dplyr::filter(date >= fired[f,]$first_date_7)%>%
-      dplyr::filter(date <= fired[f,]$last_date_7)
+      dplyr::filter(date <= fired[f,]$last_date_7) %>%
+      dplyr::select(filename) %>%
+      pull()
     
-    if(nrow(goes)>0){
-      glist<- list()
-      for(g in 1:nrow(goes)) {
-        glist[[g]] <- read_csv(file.path("data","goes16",goes$filename[g]), col_types = c("TTdddddddddd"))%>%
-            dplyr::select(-cellindex, -x,-y,-Area,-Temp,-Power, -DQF,-scan_center)
-      }
-
-      ints <- bind_rows(glist) %>%
-        st_as_sf(coords=c("sinu_x", "sinu_y"), crs=st_crs(fired)) %>%
-        mutate(is_fire = st_intersects(big_thing, fired[f,],sparse = F) %>%
+    if(length(goes)>0){
+      tbl <- goes %>% 
+        map_df(~read_csv(., col_types = c("TTdddddddddd")))%>%
+        dplyr::select(rounded_datetime, Mask, sinu_x, sinu_y)%>%
+        st_as_sf(coords=c("sinu_x", "sinu_y"), crs=st_crs(fired))%>%
+        mutate(is_fire = st_intersects(., fired[f,],sparse = F) %>%
                  rowSums()) %>%
         filter(is_fire > 0) 
       
       if(nrow(ints)>0){
-        fire_counts[[counter]] <- ints %>%
+        fire_counts <- ints %>%
           st_set_geometry(NULL) %>%
           group_by(rounded_datetime) %>%
           dplyr::summarise(n=n()) %>%
           mutate(nid = fired[f,]$nid)
-        counter<-counter+1
-        system(paste("echo", counter))
+        return(fire_counts)
         
       }
     }
   }
-  bind_rows(fire_counts) %>%
-    write_csv(file.path("data","out", out_file))
+  # bind_rows(fire_counts) %>%
+  write_csv(fc,file.path("data","out", out_file))
   system(paste0("aws s3 cp ", file.path("data","out", out_file), 
                 " s3://earthlab-amahood/night_fires/goes_counts/", out_file
-                ))
+  ))
 }
