fc7a64490cc892bc2f75f463f33246bd5f837a27
‘admahood’ <admahood@gmail.com>
2020-10-26 23:28:01 +0000

    k

--- r/download_landcover.r
+++ r/download_landcover.r
@@ -31,26 +31,26 @@ tiles <- dir_listing1$tiles %>% unique
 #           "h08v05","h09v05","h10v05","h11v05","h12v05",
 #           "h08v06","h09v06","h10v06","h11v06")
 
-tiles <- c("h13v14" ,"h14v14" ,"h12v13" ,"h13v13" ,"h11v12" ,"h12v12", "h13v12", "h08v11", "h11v11", "h12v11",
-            "h13v11", "h14v11", "h10v10", "h11v10", "h12v10", "h13v10", "h14v10", "h08v09", "h09v09", "h10v09",
-            "h11v09", "h12v09", "h13v09", "h14v09", "h08v08", "h09v08", "h10v08", "h11v08", "h12v08", "h13v08",
-            "h03v07", "h07v07", "h08v07", "h09v07", "h10v07", "h11v07", "h12v07", "h03v06", "h07v06", "h08v06",
-            "h09v06", "h10v06", "h11v06", "h07v05", "h08v05", "h09v05", "h10v05", "h11v05", "h12v05", "h08v04",
-            "h09v04", "h10v04", "h11v04", "h12v04", "h13v04", "h14v04", "h06v03", "h07v03", "h08v03", "h09v03",
-            "h10v03", "h11v03", "h12v03", "h13v03", "h14v03", "h15v03", "h28v03", "h29v03", "h09v02", "h10v02",
-            "h11v02", "h12v02", "h13v02", "h14v02", "h15v02", "h16v02", "h17v02", "h12v01", "h13v01", "h14v01",
-            "h15v01", "h16v01", "h17v01", "h16v00", "h17v00")
+#western hemi
+# tiles <- c("h13v14" ,"h14v14" ,"h12v13" ,"h13v13" ,"h11v12" ,"h12v12", "h13v12", "h08v11", "h11v11", "h12v11",
+#             "h13v11", "h14v11", "h10v10", "h11v10", "h12v10", "h13v10", "h14v10", "h08v09", "h09v09", "h10v09",
+#             "h11v09", "h12v09", "h13v09", "h14v09", "h08v08", "h09v08", "h10v08", "h11v08", "h12v08", "h13v08",
+#             "h03v07", "h07v07", "h08v07", "h09v07", "h10v07", "h11v07", "h12v07", "h03v06", "h07v06", "h08v06",
+#             "h09v06", "h10v06", "h11v06", "h07v05", "h08v05", "h09v05", "h10v05", "h11v05", "h12v05", "h08v04",
+#             "h09v04", "h10v04", "h11v04", "h12v04", "h13v04", "h14v04", "h06v03", "h07v03", "h08v03", "h09v03",
+#             "h10v03", "h11v03", "h12v03", "h13v03", "h14v03", "h15v03", "h28v03", "h29v03", "h09v02", "h10v02",
+#             "h11v02", "h12v02", "h13v02", "h14v02", "h15v02", "h16v02", "h17v02", "h12v01", "h13v01", "h14v01",
+#             "h15v01", "h16v01", "h17v01", "h16v00", "h17v00")
 
-years = 2016:2019 #2017 disappeared!!
+years = 2001:2019 #2017 disappeared!!
 
 local_data <- "data/MCD12Q1"
-s3_path <- "s3://earthlab-natem/modis-burned-area/input/landcover"
+s3_path <- "s3://earthlab-amahood/night_fires/landcover"
 
 # corz <- detectCores()-1
 # registerDoParallel(corz)
 for (y in 1:length(years)) {
-  
-  
+  print(years[y])
   url1 <-paste0("https://e4ftl01.cr.usgs.gov/MOTA/MCD12Q1.006/", years[y], ".01.01/")
   
   existing_files <- system(paste0("aws s3 ls ",
@@ -97,12 +97,11 @@ for (y in 1:length(years)) {
     
     system(paste(
       "aws s3 sync",
-      file.path(local_data,years[y]),
-      file.path("s3://earthlab-natem/modis-burned-area/input/landcover",years[y])
+      file.path(local_data, years[y]),
+      file.path(s3_path, years[y])
     ))
     unlink(file.path(local_data, years[y]), recursive = TRUE)
     print(Sys.time() - t0)
-    print(years[y])
   }
 }
 
