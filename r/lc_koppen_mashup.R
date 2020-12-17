6c9fbf6ea6dd22e3ab418501a504867e9f974d39
‘admahood’ <admahood@gmail.com>
2020-12-17 18:55:18 +0000

    j

--- r/lc_koppen_mashup.R
+++ r/lc_koppen_mashup.R
@@ -33,6 +33,13 @@ lut_kop <- c("Af",  "Am", "Aw",
 lut_kop <- c(rep(1,3), rep(2,4), rep(3,9), rep(4,12), rep(5,2))
 names(lut_kop)<-c(1:30)
 
+# get template
+template_path_s3 <- "s3://earthlab-amahood/night_fires/test1.nc"
+template_path_local <- "data/test1.nc"
+system(paste("aws s3 cp",
+             template_path_s3,
+             template_path_local))
+
 # data ingest =================
 template <-raster("data/test1.nc") %>%
   st_as_stars()
@@ -41,19 +48,18 @@ template <-raster("data/test1.nc") %>%
 lc <- read_stars(file.path(lc_path_local, fn)) %>%
   st_warp(dest = template)
 
-kop <- raster('data/koppen/Beck_KG_V1_present_0p0083.tif') %>%
-  as.data.frame(xy=TRUE)
-
-# take a while so we'll save it
-# save(lc,kop, file="data/lc_kop.Rda")
-# }else{load("data/lc_kop.Rda")}
-
+kop <- raster('data/koppen/Beck_KG_V1_present_0p0083.tif') 
 # reclassifying the kop the old-fashioned way
 kop[kop<4] <- 1
 kop[kop>3 & kop<8] <- 2
 kop[kop>7 & kop<17] <- 3
 kop[kop>16 & kop < 29] <- 4
 kop[kop > 28] <- 5
+# take a while so we'll save it
+# save(lc,kop, file="data/lc_kop.Rda")
+# }else{load("data/lc_kop.Rda")}
+
+
 # # SANITY CHECK
 # freq(kop)
 # freq(lc)
