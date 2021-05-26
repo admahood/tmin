1fb43cc2e9d06ecc23e19e675ab98c1cc8781d0b
‘admahood’ <admahood@gmail.com>
2021-05-26 16:04:27 -0600

    k

--- r/koppen_lc_tables.R
+++ r/koppen_lc_tables.R
@@ -59,9 +59,141 @@ total_land_area = lck_tab %>% pull(area_km2) %>% sum
 burnable_land_area = thresholds %>% pull(area_km2) %>% sum()
 burnable_land_area/(total_land_area)
 
-xxx<-st_crs(read_stars("data/adjusted_counts/op-adjusted_D_200301.tif"))
-yyy<-st_crs(lck)
+library(raster)
+xxx<-raster("data/adjusted_counts/op-adjusted_D_200301.tif")
+yyy<-raster("data/lc_koppen_2010_mode.tif")
 xxx==yyy
 
 # getting extended table 1 stats right here baby (michael's script has a lot of 
 # adjusting that is no longer necessary)
+
+# doing the overpass adjustment for 0.25 degrees================================
+dir.create("data/adjusted_counts_025")
+
+# getting files
+system(paste("aws s3 sync",
+             "s3://earthlab-jmcglinchy/night_fire/gridded/vars_refresh_may2021/CSV_nocorn_grid_0_25_degree_vars",
+             "data/gridded_mod14_025"))
+
+system(paste("aws s3 sync",
+             "s3://earthlab-mkoontz/MODIS-overpass-counts_0.25_analysis-ready/month_2003-2020/",
+             "data/overpass_counts_025"))
+
+# creating the lists of files
+op_days <- list.files("data/overpass_counts_025/month_2003-2020", 
+                      full.names = TRUE, pattern = "*day*") 
+op_nights <- list.files("data/overpass_counts_025/month_2003-2020", 
+                        full.names = TRUE, pattern = "*night*")
+
+mod14_day_counts_025<- list.files("data/gridded_mod14_025/AFC_num", 
+                              full.names = TRUE,
+                              pattern = "_D_") %>%
+  as_tibble() %>%
+  mutate(year = str_extract(value, "\\d{4}") %>% as.numeric) %>%
+  filter(year > 2002)%>%
+  separate(value, sep = "_", into = c("g1", "g2","g3","g4","g5","g7","month","g6"), remove = FALSE) %>%
+  dplyr::select(-starts_with("g")) %>%
+  mutate(date = as.Date(paste(year, month, "01", sep="-"), "%Y-%B-%d"),
+         month_n = lubridate::month(date))
+
+mod14_night_counts_025<- list.files("data/gridded_mod14_025/AFC_num", 
+                                   full.names = TRUE,
+                                   pattern = "_N_") %>%
+  as_tibble  %>%
+  mutate(year = str_extract(value, "\\d{4}") %>% as.numeric) %>%
+  filter(year > 2002) %>%
+  separate(value, sep = "_", into = c("g1", "g2","g3","g4","g5","g7","g8","g9","month","g6"), remove = FALSE) %>%
+  dplyr::select(-starts_with("g")) %>%
+  mutate(date = as.Date(paste(year, month, "01", sep="-"), "%Y-%B-%d"),
+         month_n = lubridate::month(date)) 
+
+# doing the adjusting in parallel ===========
+
+
+registerDoParallel(detectCores()-1)
+foreach(i = 1:nrow(mod14_day_counts_025))%dopar%{
+  system(paste("echo",i))
+  month <- mod14_day_counts_025$month_n[i]
+  
+  counts <- raster::raster(mod14_day_counts_025$value[i])
+  overpasses<- raster::raster(op_days_025[month])
+  
+  adjusted <- counts/overpasses
+  outfile <- paste0("data/adjusted_counts_025/op-adjusted_D_", 
+                    mod14_day_counts_025$year[i],
+                    str_pad(month, width=2, side="left", pad = "0"),
+                    ".tif"
+  )
+  writeRaster(adjusted,outfile, overwrite=TRUE)  
+  
+  month_n <- mod14_night_counts_025$month_n[i]
+  
+  counts_n <- raster::raster(mod14_night_counts_025$value[i])
+  overpasses_n <- raster::raster(op_nights_025[month_n])
+  
+  adjusted_n <- counts_n/overpasses_n
+  outfile_n <- paste0("data/adjusted_counts_025/op-adjusted_N_", 
+                      mod14_night_counts_025$year[i],
+                      str_pad(month, width=2, side="left", pad = "0"),
+                      ".tif"
+  )
+  writeRaster(adjusted_n,outfile_n, overwrite=TRUE)
+  
+  
+  if(str_extract(outfile, "\\d{6}")==str_extract(outfile_n, "\\d{6}")){
+    
+    nf<- adjusted_n/(adjusted+adjusted_n)
+    outfile_nf <- paste0("data/adjusted_counts_025/op-adjusted_NF_", 
+                         mod14_night_counts_025$year[i],
+                         str_pad(month, width=2, side="left", pad = "0"),
+                         ".tif"
+    )
+    writeRaster(nf,outfile_nf, overwrite=TRUE)}
+}
+
+# aggregation ==============
+
+adjusted_n <- list.files("data/adjusted_counts_25", full.names = TRUE, pattern = "*_N_*") %>%
+  as_tibble() %>%
+  mutate(year = str_extract(value,"\\d{4}"))
+
+adjusted_d <- list.files("data/adjusted_counts_25", full.names = TRUE, pattern = "*_D_*")%>%
+  as_tibble() %>%
+  mutate(year = str_extract(value,"\\d{4}"))
+
+frp <- list.files("data/gridded_mod14_2_5/FRP_mean", pattern = "*_N_*", full.names = TRUE) %>%
+  as_tibble() %>%
+  mutate(year = str_extract(value,"\\d{4}"))
+
+dir.create("data/annual_adjusted_counts_25")
+years <- 2003:2020
+for(y in years){
+  print(y)
+  d<-filter(adjusted_d, year == y) %>%
+    pull(value) %>%
+    raster::stack() %>%
+    sum
+  writeRaster(d, 
+              filename = paste0("data/annual_adjusted_counts_25/day_adj_annual_counts_",y, ".tif"),
+              overwrite = TRUE)
+  n<-filter(adjusted_n, year == y) %>%
+    pull(value) %>%
+    raster::stack() %>%
+    sum
+  writeRaster(n, 
+              filename = paste0("data/annual_adjusted_counts_25/night_adj_annual_counts_",y, ".tif"),
+              overwrite = TRUE)
+  nf<- n/(n+d)
+  writeRaster(nf, 
+              filename = paste0("data/annual_adjusted_counts_25/night_fraction_adj_annual_counts_",y, ".tif"),
+              overwrite = TRUE)
+  
+  ff <- filter(frp, year == y) %>%
+    pull(value) %>%
+    raster::stack() %>%
+    mean
+  writeRaster(ff, 
+              filename = paste0("data/annual_adjusted_counts_25/night_frp_annual_mean_",y, ".tif"),
+              overwrite = TRUE)
+  
+}
